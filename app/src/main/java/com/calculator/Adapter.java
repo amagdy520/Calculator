f7ff1d4de8aedebf87895b840b50792179647300
Ahmed Magdy Abd-alzaher <ahmed.magdy.abd.alzaher@gmail.com>
2018-02-11 03:54:50 +0200

    Initial commit

---
+++ app/src/main/java/com/calculator/Adapter.java
@@ -0,0 +1,31 @@
+package com.calculator;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.util.Log;
+
+
+/**
+ * Created by Ahmed Magdy on 2/8/2018.
+ */
+
+public class Adapter extends FragmentPagerAdapter {
+    private final String title[] = new String[]{"Main","Other"};
+    private final Fragment fragment[] = new Fragment[title.length];
+    public Adapter(FragmentManager fm) {
+        super(fm);
+        fragment[0] = new FragmentMain();
+        fragment[1] = new FragmentOther();
+    }
+    @Override
+    public Fragment getItem(int position) {
+        Log.v("Title - get Item = ", String.valueOf(position));
+        return fragment[position];
+    }
+
+    @Override
+    public int getCount() {
+        return fragment.length;
+    }
+}
---
+++ app/src/main/java/com/calculator/FragmentOther.java
@@ -0,0 +1,21 @@
+package com.calculator;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Ahmed Magdy on 2/8/2018.
+ */
+
+public class FragmentOther extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_other,container,false);
+        return view;
+    }
+}
